6fdaada623e03ec397b4d579fc7ca0675a761b0b
♔•アરเέຮ┼•♔ ぶ <streetdemonic003@gmail.com>
2024-03-15 17:49:30 -0300

    Update antispam.py

--- Powers/plugins/antispam.py
+++ Powers/plugins/antispam.py
@@ -24,12 +24,12 @@ SUPPORT_STAFF = get_support_staff()
 async def gban(c: Gojo, m: Message):
     if len(m.text.split()) == 1:
         await m.reply_text(
-            text="<b>How to gban?</b> \n <b>Answer:</b> <code>/gban user_id reason</code>"
+            text="<b>Como gban?</b> \n <b>Responder:</b> <code>/gban user_id reason</code>"
         )
         return
 
     if len(m.text.split()) == 2 and not m.reply_to_message:
-        await m.reply_text(text="Please enter a reason to gban user!")
+        await m.reply_text(text="Por favor, insira um motivo para o usuário gban!")
         return
 
     user_id, user_first_name, _ = await extract_user(c, m)
@@ -40,41 +40,41 @@ async def gban(c: Gojo, m: Message):
         gban_reason = m.text.split(None, 2)[2]
 
     if user_id in SUPPORT_STAFF:
-        await m.reply_text(text="This user is part of my Support!, Can't ban our own!")
+        await m.reply_text(text="Este usuário faz parte do meu Suporte!, Não podemos banir os nossos!")
         return
 
     if user_id == Config.BOT_ID:
         await m.reply_text(
-            text="You don't dare use that command on me again nigga! \n Go straight and fuck your self......"
+            text="Você não se atreve a usar esse comando em mim novamente nigga! \n Vá direto e foda-se......"
         )
         return
 
     if db.check_gban(user_id):
         db.update_gban_reason(user_id, gban_reason)
-        await m.reply_text(text="Updated Gban reason to: <code>{gban_reason}</code>.")
+        await m.reply_text(text="Razão Gban atualizada para: <code>{gban_reason}</code>.")
         return
 
     db.add_gban(user_id, gban_reason, m.from_user.id)
     await m.reply_text(
         (
-            f"Added {user_first_name} to GBan List. \n They will now be banned in all groups where I'm admin!"
+            f"Adicionado {user_first_name} para Lista GBan. \n Eles agora serão banidos em todos os grupos onde eu sou administrador!"
         )
     )
-    LOGGER.info(f"{m.from_user.id} gbanned {user_id} from {m.chat.id}")
+    LOGGER.info(f"{m.from_user.id} gbanido {user_id} De {m.chat.id}")
     date = datetime.utcnow().strftime("%H:%M - %d-%m-%Y")
-    log_msg = f"#GBAN \n <b>Originated from:</b> {m.chat.id} \n <b>Admin:</b> {await mention_html(m.from_user.first_name, m.from_user.id)} \n <b>Gbanned User:</b> {await mention_html(user_first_name, user_id)} \n <b>Gbanned User ID:</b> {user_id} \\ n<b>Event Stamp:</b> {date}"
+    log_msg = f"#GBAN \n <b>Originado de:</b> {m.chat.id} \n <b>Admin:</b> {await mention_html(m.from_user.first_name, m.from_user.id)} \n <b>Gbanido User:</b> {await mention_html(user_first_name, user_id)} \n <b>Gbanned User ID:</b> {user_id} \\ n<b>Event Stamp:</b> {date}"
     await c.send_message(MESSAGE_DUMP, log_msg)
     try:
         # Send message to user telling that he's gbanned
         await c.send_message(
             user_id,
-            f"You have been added to my global ban list! \n <b>Reason:</b> <code>{gban_reason}</code> \n <b>Appeal Chat:</b> @{SUPPORT_GROUP}",
+            f"Você foi adicionado à minha lista de banimento global! \n <b>Razão:</b> <code>{gban_reason}</code> \n <b>Chat de Apelação:</b> @{SUPPORT_GROUP}",
         )
     except UserIsBlocked:
-        LOGGER.error("Could not send PM Message, user blocked bot")
+        LOGGER.error("Não foi possível enviar mensagem PM, usuário bloqueado bot")
     except PeerIdInvalid:
         LOGGER.error(
-            "Haven't seen this user anywhere, mind forwarding one of their messages to me?",
+            "Não vi este usuário em lugar nenhum, mente encaminhar uma de suas mensagens para mim?",
         )
     except Exception as ef:  # TO DO: Improve Error Detection
         LOGGER.error(ef)
@@ -87,46 +87,46 @@ async def gban(c: Gojo, m: Message):
 )
 async def ungban(c: Gojo, m: Message):
     if len(m.text.split()) == 1:
-        await m.reply_text(text="Pass a user id or username as an argument!")
+        await m.reply_text(text="Passar um ID de usuário ou nome de usuário como um argumento!")
         return
 
     user_id, user_first_name, _ = await extract_user(c, m)
 
     if user_id in SUPPORT_STAFF:
-        await m.reply_text(text="This user is part of my Support!, Can't ban our own!")
+        await m.reply_text(text="Este usuário faz parte do meu Suporte!, Não posso banir o nosso!")
         return
 
     if user_id == Config.BOT_ID:
         await m.reply_text(
-            text="""You can't gban me nigga!
-        Fuck yourself.......!"""
+            text="""Você não pode gban nigga!
+        Foda-se.......!"""
         )
         return
 
     if db.check_gban(user_id):
         db.remove_gban(user_id)
-        await m.reply_text(text=f"Removed {user_first_name} from Global Ban List.")
+        await m.reply_text(text=f"Removido {user_first_name} da Lista Global de Banimento.")
         time = ((datetime.utcnow().strftime("%H:%M - %d-%m-%Y")),)
-        LOGGER.info(f"{m.from_user.id} ungbanned {user_id} from {m.chat.id}")
+        LOGGER.info(f"{m.from_user.id} ungbanned {user_id} De {m.chat.id}")
         log_msg = f"""#UNGBAN
-        <b>Originated from:</b> {m.chat.id}
+        <b>Originado de:</b> {m.chat.id}
         <b>Admin:</b> {(await mention_html(m.from_user.first_name, m.from_user.id))}
         <b>UnGbanned User:</b> {(await mention_html(user_first_name, user_id))}
         <b>UnGbanned User ID:</b> {user_id}
-        <b>Event Stamp:</b> {time}"""
+        <b>Carimbo do Evento:</b> {time}"""
         await c.send_message(MESSAGE_DUMP, log_msg)
         try:
             # Send message to user telling that he's ungbanned
             await c.send_message(
                 user_id,
-                text="You have been removed from my global ban list!.....Be careful it takes few seconds to add you again...",
+                text="Você foi removido da minha lista de banimento global..... Tenha cuidado, leva alguns segundos para adicioná-lo novamente...",
             )
         except Exception as ef:  # TODO: Improve Error Detection
             LOGGER.error(ef)
             LOGGER.error(format_exc())
         return
 
-    await m.reply_text(text="User is not gbanned!")
+    await m.reply_text(text="Usuário não é gbanido!")
     return
 
 
@@ -135,7 +135,7 @@ async def ungban(c: Gojo, m: Message):
 )
 async def gban_count(_, m: Message):
     await m.reply_text(
-        text=f"Number of people gbanned: <code>{(db.count_gbans())}</code>"
+        text=f"Número de pessoas gbanidas: <code>{(db.count_gbans())}</code>"
     )
     LOGGER.info(f"{m.from_user.id} counting gbans in {m.chat.id}")
     return
@@ -148,14 +148,14 @@ async def gban_list(_, m: Message):
     banned_users = db.load_from_db()
 
     if not banned_users:
-        await m.reply_text(text="There aren't any gbanned users...!")
+        await m.reply_text(text="Não há usuários gbanidos...!")
         return
 
-    banfile = "Here are all the globally banned geys!\n\n"
+    banfile = "Aqui estão todos os g4ys globalmente proibidos!\n\n"
     for user in banned_users:
         banfile += f"[x] <b>{Users.get_user_info(user['_id'])['name']}</b> - <code>{user['_id']}</code>\n"
         if user["reason"]:
-            banfile += f"<b>Reason:</b> {user['reason']}\n"
+            banfile += f"<b>Razão:</b> {user['reason']}\n"
 
     try:
         await m.reply_text(banfile)
@@ -163,9 +163,9 @@ async def gban_list(_, m: Message):
         with BytesIO(str.encode(await remove_markdown_and_html(banfile))) as f:
             f.name = "gbanlist.txt"
             await m.reply_document(
-                document=f, caption="Here are all the globally banned geys!\n\n"
+                document=f, caption="Aqui estão todos os g4ys globalmente proibidos!\n\n"
             )
 
-    LOGGER.info(f"{m.from_user.id} exported gbanlist in {m.chat.id}")
+    LOGGER.info(f"{m.from_user.id} gbanlist exportado em {m.chat.id}")
 
     return
